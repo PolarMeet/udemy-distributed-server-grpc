b1ef0ae590a2d0f2ccb076473d972b4bf68851a0
Chinmay Anand <chinmayanand896@gmail.com>
2024-12-11 22:21:41 +0900

    initial commit

---
+++ src/main/kotlin/org/polarmeet/grpcdistributedserverudemy/GrpcDistributedServerUdemyApplication.kt
@@ -0,0 +1,11 @@
+package org.polarmeet.grpcdistributedserverudemy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpcDistributedServerUdemyApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrpcDistributedServerUdemyApplication>(*args)
+}
