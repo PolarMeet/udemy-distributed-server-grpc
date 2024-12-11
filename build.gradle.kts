e643d83748a17175e56cd9a8bf39e3d19ff41781
Chinmay Anand <chinmayanand896@gmail.com>
2024-12-11 23:09:53 +0900

    UC-1 add dependencies to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
+import java.util.random.RandomGeneratorFactory.all
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "org.polarmeet"
 version = "0.0.1-SNAPSHOT"
+
+// define version in one place for better maintenance
+val grpcVersion = "1.60.0"
+val grpcKotlinVersion = "1.4.1"
+val protobufVersion = "3.25.1"
+val coroutinesVersion = "1.7.3"
 
 java {
     toolchain {
@@ -24,10 +33,45 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+
+    // grpc
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    // testing
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+        }
+    }
 }
 
 kotlin {
